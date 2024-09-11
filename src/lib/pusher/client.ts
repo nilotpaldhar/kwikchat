72646f4aa8ce2afe171f5a2389c6eff2a11edede
Nilotpal Dhar <nilotpaldhar1997@gmail.com>
2024-09-11 19:36:29 +0530

    fix(pusher): fix pusher client initialization error

--- src/lib/pusher/client.ts
+++ src/lib/pusher/client.ts
@@ -6,7 +6,7 @@ import PusherClient from "pusher-js";
 
 // Creating a new Pusher client instance using environment variables for configuration.
 // The client will connect to Pusher to subscribe to channels and receive real-time events.
-const pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
+const pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY ?? "", {
 	cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER ?? "ap2",
 });
 
