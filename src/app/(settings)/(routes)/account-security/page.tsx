52eb8eb51f987b37bb372fbde59c96e7a9e6fa70
Nilotpal Dhar <nilotpaldhar1997@gmail.com>
2025-01-16 19:31:22 +0530

    feat(seo): enhance SEO for settings-related routes

--- src/app/(settings)/(routes)/account-security/page.tsx
+++ src/app/(settings)/(routes)/account-security/page.tsx
@@ -1,3 +1,5 @@
+import type { Metadata } from "next";
+
 import Header from "@/app/(settings)/_components/header";
 import SettingsContent from "@/app/(settings)/_components/settings-content";
 import UpdatePassword from "@/app/(settings)/_components/update-password";
@@ -5,6 +7,12 @@ import ToggleTwoFactorAuth from "@/app/(settings)/_components/toggle-two-factor-
 
 import { getSession } from "@/data/auth/session";
 import { getCachedUserById } from "@/data/user";
+
+export const metadata: Metadata = {
+	title: "Account Security",
+	description:
+		"Manage your password settings and enhance account protection with two-factor authentication.",
+};
 
 const AccountSecurity = async () => {
 	const session = await getSession();
